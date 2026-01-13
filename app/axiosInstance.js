aa3a2ec8319b69af11cf4b75294215aae459c077
Tuguldur <tuguldur8000@gmail.com>
2026-01-13 16:56:50 +0800

    Last push

--- app/axiosInstance.js
+++ app/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // Хэрэв локал дээр ажиллаж байвал localhost, онлайнд байвал Render-ийн хаяг ашиглана
   baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://tanii-backend.onrender.com' 
+    ? 'https://purenest-app.onrender.com' 
     : 'http://localhost:4000',
   headers: {
     'Content-Type': 'application/json'
